8f148d209005688b2d6bd695e34ed605ae6b07f3
Mac <mac@192.168.80.23>
2025-12-12 18:41:19 -0500

    ok1

--- models/User.js
+++ models/User.js
@@ -1,35 +1,48 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /.+\@.+\..+/,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  name: String,
-  lastName: String,
-  document: String,
-  gender: String,
-  birthdate: Date, // antes era String
-  phone: {
-    type: String,
-    match: /^[0-9\-\+]{9,15}$/,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
 
-  address: {
-    street: String,
-    city: String,
-    state: String,
-    zip: String,
-    country: String,
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    name: String,
+    lastName: String,
+    document: String,
+    gender: String,
+    birthdate: Date,
+
+    phone: {
+      type: String,
+      match: /^[0-9\-\+]{9,15}$/,
+    },
+
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      zip: String,
+      country: String,
+    },
+
+    /** 🆕 PEDIDOS DEL USUARIO */
+    orders: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Order",
+      default: [], // ← ESTO evita el error del webhook
+    },
   },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
